ea87dec1a2c7a7360ccde2c93321c8650e7e7a43
Agent1c <medicenyb@gmail.com>
2024-11-24 23:37:48 +0200

    Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/relationship_app/forms.py
+++ advanced_features_and_security/LibraryProject/relationship_app/forms.py
@@ -3,14 +3,14 @@ from .models import Book
 from django.shortcuts import render
 
 
-class BookForm(forms.ModelForm):
+class ExampleForm(forms.ModelForm):
     
     class Meta:
          fields = ('title', 'author')
 
 # In your view
 def search_books(request):
-    form = BookForm(request.GET or None)
+    form = ExampleForm(request.GET or None)
     if form.is_valid():
         query = form.cleaned_data['query']
         results = Book.objects.filter(title__icontains=query)
