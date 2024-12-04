55d0bdd8ed6475a4cf1f8f208192b5839c841502
Agent1c <medicenyb@gmail.com>
2024-12-04 19:26:59 +0200

    0. Setting Up a New Django Project with Custom Serializers in Django REST Framework

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -5,8 +5,9 @@ from django.db import models
 #  Authors Model
 class Author(models.Model):
     name = models.CharField(_("name"), max_length=50)
-
-    pass
+    
+    def __str__(self) -> str:
+        return self.name
 
 # Book Model
 class Book(models.Model):
@@ -14,3 +15,11 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     publication_year = models.IntegerField("2024-05-24")
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    related_books = models.ManyToManyField('self', blank=True)
+
+    def __str__(self):
+        return self.title
---
+++ advanced_api_project/api/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Book
+
+
+# BookSerializer that serializes all fields of the Book model
+class BookSerializer(serializers.ModelSerializer):
+    # Define the nested serializer for related books
+    related_books = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['name']
