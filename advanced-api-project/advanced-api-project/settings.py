266dfdc7d7b6b994f19a195eab8a5650befc2299
Agent1c <medicenyb@gmail.com>
2024-12-04 21:30:36 +0200

    0. Setting Up a New Django Project with Custom Serializers in Django REST Framework

--- advanced-api-project/advanced-api-project/settings.py
+++ advanced-api-project/advanced-api-project/settings.py
@@ -32,14 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
+    'api',
+    'django_filters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
-    'django_filters',
 ]
 
 MIDDLEWARE = [
