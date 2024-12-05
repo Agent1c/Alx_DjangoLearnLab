771950a535a2618be1be683d5ac6598699e27d2a
Agent1c <medicenyb@gmail.com>
2024-12-05 06:19:20 +0200

    published_date

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -17,7 +17,7 @@ class User(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    date_posted = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
 
     def __str__(self) -> str:
