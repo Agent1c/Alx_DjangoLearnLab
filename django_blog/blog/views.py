47cdc45a5ea30853aeb740746d1ef3441dd31530
Agent1c <medicenyb@gmail.com>
2024-12-08 20:13:09 +0200

    1. Implementing the Blog's User Authentication System

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+# register view model
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login') # Redirect to login page after registration
+        else:
+            form = CustomUserCreationForm()
+        return render(request, 'blog/register.html', {'form' : form})
+    
+
+# simple profile view where users can see their details.
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html', {'user' : request.user})
+    
